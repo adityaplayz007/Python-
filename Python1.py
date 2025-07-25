96a8bb9c4057c34e970d9e867e93b82c8e276e0d
adityaplayz007 <adityaprimeera007@gmail.com>
2025-07-25 17:16:25 +0530

    Add files via upload

---
+++ Python1.py
@@ -0,0 +1,22 @@
+print("Welcome User Let Me know You First")
+print("Let's Get started")
+name = str(input("What's Your Name?   "))
+age = int(input("So,What's your age ? :   "))
+height = float(input("Your Height?  "))
+fav_num = int(input("What's fav. Number?  "))
+Mo_no = int(input("Give me your Contact  "))
+email = str(input("Enter your email id"))
+
+print("Thanks Sir ! This is Your Data")
+
+print("Your Name is ",type(name), id(name))
+print("You're",age,"Years Old",type(age), id(age))
+print("Your height is ", type(height), id(height))
+print("your favourit number is " , type(fav_num), id(fav_num))
+print("Your Contact is " , type(Mo_no), id(Mo_no))
+print("your email is ", type(email), id(email))
+
+birth_year = 2025 - age
+print("Your Birth is in year ", birth_year)
+
+print("thanks to use our Web")
